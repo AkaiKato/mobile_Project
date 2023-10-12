7a482b01e833a8f7ba85b2ba29ed6a9abc88b3db
rail khabirov <r.khabirov@krialenergo.ru>
2023-10-12 17:01:20 +0300

    End with login & pass f-d add sign in btn

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -10,6 +10,90 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage > {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 25,),
+              const Text(
+                "Hello again fella!",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold, 
+                  fontSize: 24
+                ),
+              ),
+
+              const SizedBox(height: 20,),
+
+              //Login field
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.only(left: 20.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Contractor Name',
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 10,),
+
+              //Password field
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.only(left: 20.0),
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Password',
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 10,),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                  padding: const EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      'Sign in',
+                      style: TextStyle(color: Colors.white),  
+                    ),
+                  ),
+                ),
+              )
+            ]
+          ),
+        )
+      ),
+    );
   }
 }
