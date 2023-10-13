ecde52dd3952dbda70749e84d0a10be3632f012b
rail khabirov <r.khabirov@krialenergo.ru>
2023-10-13 14:01:42 +0300

    New Start

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,11 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-     debugShowCheckedModeBanner: false,
-     home: LoginPage(),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
     );
   }
 }
-
-
-
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LogiinPage extends StatefulWidget {
+  const LogiinPage({super.key});
+
+  @override
+  State<LogiinPage> createState() => _LogiinPageState();
+}
+
+class _LogiinPageState extends State<LogiinPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
