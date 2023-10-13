0801b0f752df57bb369882d05843afb2959e504c
rail khabirov <r.khabirov@krialenergo.ru>
2023-10-13 16:01:32 +0300

    new logo, w/Words, login TF, pass TF

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,15 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_project/components/my_textfield.dart';
 
-class LogiinPage extends StatefulWidget {
-  const LogiinPage({super.key});
+class LoginPage extends StatelessWidget {
+  TextEditingController loginController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
-  @override
-  State<LogiinPage> createState() => _LogiinPageState();
-}
+  LoginPage({super.key});
 
-class _LogiinPageState extends State<LogiinPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //logo
+              Container(
+                child: Image.asset('lib/Images/favicon.png'),
+              ),
+
+              const SizedBox(
+                height: 20,
+              ),
+
+              //Greetings
+              const Text(
+                'Приветствуем в нашем приложении!',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+
+              const SizedBox(
+                height: 20,
+              ),
+
+              //login textfield
+              MyTextField(
+                hintText: "Логин",
+                obscureText: false,
+                controller: loginController,
+              ),
+
+              const SizedBox(
+                height: 10,
+              ),
+
+              //pass textfield
+              MyTextField(
+                hintText: "Пароль",
+                obscureText: true,
+                controller: passwordController,
+              ),
+
+              const SizedBox(
+                height: 10,
+              ),
+
+              //Text Most Common QS
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    "Часто задаваемые вопросы",
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 13, 20, 207),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
