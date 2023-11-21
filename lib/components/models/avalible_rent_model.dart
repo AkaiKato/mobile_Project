f1577446576a175b76358dded9a8903306dd18ef
rail khabirov <r.khabirov@krialenergo.ru>
2023-11-21 14:40:52 +0300

    add avalible_rend_model

---
+++ lib/components/models/avalible_rent_model.dart
@@ -0,0 +1,37 @@
+class AvalibleRent {
+  String modelName;
+  String modelEngine;
+  String powerNominal;
+  String dguType;
+  String size;
+  String weight;
+  String yearIssue;
+  String costAt100perc;
+  String fuelVolume;
+
+  AvalibleRent({
+    required this.modelName,
+    required this.modelEngine,
+    required this.powerNominal,
+    required this.dguType,
+    required this.size,
+    required this.weight,
+    required this.yearIssue,
+    required this.costAt100perc,
+    required this.fuelVolume,
+  });
+
+  factory AvalibleRent.fromJson(Map<dynamic, dynamic> json) {
+    return AvalibleRent(
+      modelName: json['model'],
+      modelEngine: json['stationParm']['modelEngine'],
+      powerNominal: json['stationParm']['powerNominal'],
+      dguType: json['stationParm']['typeDgu'],
+      size: json['stationParm']['size'],
+      weight: json['stationParm']['weight'],
+      yearIssue: json['stationParm']['yearIssue'],
+      costAt100perc: json['stationParm']['rashodAt100'],
+      fuelVolume: json['stationParm']['fuelVolume'],
+    );
+  }
+}
